31eccb046c6c413431e8b6f447101e9e6fd67752
Joshua Sabol <joshsab01@vt.edu>
2026-02-10 00:46:52 -0500

    first commit

---
+++ src/main/java/com/fafsademo/edit/FafsaEditApplication.java
@@ -0,0 +1,13 @@
+package com.fafsademo.edit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FafsaEditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FafsaEditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fafsademo/edit/FafsaEditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fafsademo.edit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FafsaEditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
